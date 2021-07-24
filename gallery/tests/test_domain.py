743856c99afe566ba9e809ff0f7f8b926cb4f1e6
HandBoy <handersonmedeiros@gmail.com>
2021-07-24 11:20:50 -0300

    test: domain create user

--- gallery/tests/test_domain.py
+++ gallery/tests/test_domain.py
@@ -1,20 +1,34 @@
 from gallery.documents import UserModel
-from gallery.domain import login
+from gallery.domain import create_user, login
 
 
 def test_fail_user_doesnot_exists(mocker):
-    # GIVE
+    # Give
     mocker.patch("gallery.domain.UserModel.find_by_email", return_value=None)
+    # Act
     has_user = login("email@email", "123123")
-
+    # Assert
     assert not has_user
 
 
 def test_success_user_exists(mocker):
     # GIVE
-    mocker.patch("gallery.domain.UserModel.find_by_email", return_value=UserModel())
-    mocker.patch("gallery.domain.UserModel.check_encrypted_password", return_value=True)
-
+    mocker.patch(
+        "gallery.domain.UserModel.find_by_email", return_value=UserModel()
+    )
+    mocker.patch(
+        "gallery.domain.UserModel.check_encrypted_password", return_value=True
+    )
+    # Act
     has_user = login("emailTrue@email", "123123")
+    # Assert
+    assert has_user
 
+
+def test_success_create_user(mocker):
+    # GIVE
+    mocker.patch("gallery.domain.UserModel.save", return_value=True)
+    # Act
+    has_user = create_user("email", "email@email", "123123")
+    # Assert
     assert has_user
