36123a16d321730b1403dde8763ee92ef20d04c6
HandBoy <handersonmedeiros@gmail.com>
2021-07-29 23:26:59 -0300

    chore: move resources to init app

--- gallery/resources/__init__.py
+++ gallery/resources/__init__.py
@@ -13,37 +13,35 @@ from .views import (
 )
 
 
-bp = Blueprint("restapi", __name__, url_prefix="/api/v1")
-api = Api(bp)
-# User
-api.add_resource(LoginResource, "/login")
-api.add_resource(UserResource, "/users")
-api.add_resource(UserGalleriesResource, "/users/<string:user_id>/galleries")
-# Galleries
-api.add_resource(GalleriesResource, "/galleries")
-
-api.add_resource(
-    ApproverResource,
-    "/gallery/<string:gallery_id>/approver",
-)
-api.add_resource(
-    FriendGalleryResource,
-    "/gallery/<string:gallery_id>/friend",
-)
-# Pictures
-api.add_resource(
-    PicturesResource,
-    "/gallery/<string:gallery_id>/pictures",
-)
-api.add_resource(
-    PictureLikeResource,
-    "/gallery/<string:gallery_id>/pictures/<string:picture_id>/like",
-)
-api.add_resource(
-    ApprovePicturesResource,
-    "/gallery/<string:gallery_id>/pictures/<string:picture_id>/approve",
-)
-
-
 def init_app(app):
+    bp = Blueprint("restapi", __name__, url_prefix="/api/v1")
+    api = Api(bp)
+    # User
+    api.add_resource(LoginResource, "/login")
+    api.add_resource(UserResource, "/users")
+    api.add_resource(UserGalleriesResource, "/users/<string:user_id>/galleries")
+    # Galleries
+    api.add_resource(GalleriesResource, "/galleries")
+
+    api.add_resource(
+        ApproverResource,
+        "/gallery/<string:gallery_id>/approver",
+    )
+    api.add_resource(
+        FriendGalleryResource,
+        "/gallery/<string:gallery_id>/friend",
+    )
+    # Pictures
+    api.add_resource(
+        PicturesResource,
+        "/gallery/<string:gallery_id>/pictures",
+    )
+    api.add_resource(
+        PictureLikeResource,
+        "/gallery/<string:gallery_id>/pictures/<string:picture_id>/like",
+    )
+    api.add_resource(
+        ApprovePicturesResource,
+        "/gallery/<string:gallery_id>/pictures/<string:picture_id>/approve",
+    )
     app.register_blueprint(bp)
